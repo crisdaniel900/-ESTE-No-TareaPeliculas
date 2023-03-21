d7d31f01f94672b25e63fb581eac74978ab09a4d
crisdaniel900 <126824633+crisdaniel900@users.noreply.github.com>
2023-03-21 22:23:59 +0000

    Prueba2

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,33 +7,100 @@ namespace Pelicula
     public class Pelicula
     {
         //Campos
+        private string titulo;
+        private int año;
+        private string pais;
+        private string director;
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
+        public Pelicula(string titulo, int año, string pais, string director)
+     {
+        this.titulo = titulo;
+        this.año = año;
+        this.pais = pais;
+        this.director = director;
+     }
      
+        //Propiedades
+        public string Titulo 
+    { 
+        get { return titulo; } 
+        set { titulo = value; }
+    }
+    
+        public int Año
+        {
+            get {return año;}
+            set {año = value;}
+        }
+
+        public string Pais
+        {
+            get {return pais;}
+            set {pais = value;}
+        }
+
+         public string Director 
+    { 
+        get { return director; } 
+        set { director = value; }
+    }
+
+
         //Métodos
-     
         public void Imprime()
         {
-     //       Console.WriteLine($"{titulo} ({año})");
+          Console.WriteLine($"{titulo} ({año})", titulo, año);
 
         }
 
+        public void AgregaActor(Actor actor)
+      {
+        actores.Add(actor);
+      }   
+
+      public void ImprimeActores()
+      {
+        foreach (Actor actor in actores)
+        {
+            actor.Imprime();
+        }  
+      }
 
     }
 
     public class Actor
     {
         //Propiedades
+        private string name;
+        private int year;
+
+        public string Nombre
+        {
+          get { return name; }
+          set { name = value; }
+        }
+
+         public int Año
+        {
+        get { return year; }
+        set { year = value; }
+       }
 
         //Constructores
-
+        public Actor (string nombre, int año)
+        {
+            name = nombre;
+            year = año;
+        }
 
         //Métodos 
         public void Imprime()
         {
-            Console.WriteLine($"{Nombre} ({Año})");
+            Console.WriteLine($"{name} ({year})", name, year);
         }
+
     }
 
     // Puedes probar tu código en Main() pero lo importante
@@ -45,7 +112,11 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+    Pelicula p1 = new Pelicula("La La Land", 2016, "USA", "Hola" );
+    p1.AgregaActor(new Actor("Ryan Gosling", 1980));
+    p1.AgregaActor(new Actor("Emma Stone", 1988));
+
+    p1.ImprimeActores();
         }
     }
 }
